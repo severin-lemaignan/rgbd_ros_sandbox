188cb570d972fbf7cfca3fcdb6bfefc274679055
Severin Lemaignan <severin.lemaignan@brl.ac.uk>
2018-04-19 17:00:12 +0100

    Fix resize function

--- src/rgbd2vr.cpp
+++ src/rgbd2vr.cpp
@@ -9,34 +9,34 @@ using namespace std;
 /**
   * Taken from https://stackoverflow.com/questions/28562401/resize-an-image-to-a-square-but-keep-aspect-ratio-c-opencv
   */
-cv::Mat RGBD2VR::resizeKeepAspectRatio(const cv::Mat &input, const cv::Size &dstSize, const cv::Scalar &bgcolor)
+cv::Mat RGBD2VR::resizeKeepAspectRatio(const cv::Mat &input, const cv::Size &dstSize)
 {
-    if(input.empty()) return input;
+    cv::Mat output = Mat::zeros(dstSize, input.type());
 
-    cv::Mat output;
+    if(input.empty()) {
+        ROS_ERROR("Empty image passed to resizeKeepAspectRatio!");
+        return output;
+    }
+
 
     double h1 = dstSize.width * (input.rows/(double)input.cols);
     double w2 = dstSize.height * (input.cols/(double)input.rows);
 
-    // initially no borders
-    int top = 0;
-    int down = 0;
-    int left = 0;
-    int right = 0;
     if( h1 <= dstSize.height)
     {
-        // only vertical borders
-        top = (dstSize.height - h1) / 2;
-        down = top;
         cv::resize( input, output, cv::Size(dstSize.width, h1));
     }
     else
     {
-        // only horizontal borders
-        left = (dstSize.width - w2) / 2;
-        right = left;
         cv::resize( input, output, cv::Size(w2, dstSize.height));
     }
+
+    int top = (dstSize.height-output.rows) / 2;
+    int down = (dstSize.height-output.rows+1) / 2;
+    int left = (dstSize.width - output.cols) / 2;
+    int right = (dstSize.width - output.cols+1) / 2;
+
+    cv::copyMakeBorder(output, output, top, down, left, right, cv::BORDER_CONSTANT, {0,0,0});
 
     return output;
 }
